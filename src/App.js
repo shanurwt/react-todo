536e0b20466c32152710c666f5524a26b9311e53
Ayush Rawat <ayushrawat11562@gmail.com>
2022-03-20 01:54:53 +0530

    btn margin

--- src/App.js
+++ src/App.js
@@ -8,14 +8,15 @@ export default function App(){
 const [tick, setTick] = useState(true);
   return (
   <>
-  <div >
-
+  <div className='selec-btn'>
     <button onClick={()=>{setTick(false)}}>Pomodoro</button>
     <button onClick={()=>{setTick(true)}}>Todo</button>
+  </div>
+
       {tick && <TodoForm className='form' /> }
       {!tick && <Pomodoro /> }
 
-  </div>
+  
   </>
   );
 }
